7a679205f2e8ba203edcb8e0825b40d3a5da6861
DarkflameMG <deknan70@gmail.com>
2021-01-12 15:27:54 +0700

    lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,18 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+int num;
+int main()
+{
+    srand(time(0));
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    num = rand()%9;
+    cout << "You will get " << grade[num] << " in this 261102.";
+    return 0;
+}
